42fa7701306453cceffbd251e2870abe98a66372
Shrey <shreykdave2411@gmail.com>
2021-03-03 10:26:39 +0530

    add code in form

---
+++ Task7/02-03-2021/zzzz011.js
@@ -0,0 +1,91 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    jQuery.validator.addMethod("emailExt", function(value, element, param) {
+    return value.match(/^[a-zA-Z0-9_\.%\+\-]+@[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,}$/);
+},'Your E-mail is wrong');
+
+            // Wait for the DOM to be ready
+$(function() {
+    // Initialize form validation on the registration form.
+    // It has the name attribute "registration"
+    $("form[name='registration']").validate({
+        
+      // Specify validation rules
+      rules: {
+        // The key name on the left side is the name attribute
+        // of an input field. Validation rules are defined
+        // on the right side
+        firstname: "required",
+        email: { 
+            required: true,
+            emailExt: true
+        },
+        password: {
+          required: true,
+          minlength: 5
+        },
+        mobile: {
+          required: true,
+          maxlength: 10,
+          minlength: 10,
+          digits: true
+        },
+        gender:{
+            required: true,
+        },
+        country:{
+            required: true,
+        },
+        password1:{
+            required: true,
+            minlength: 5,
+            equalTo: "#password"
+        },
+        textarea:{
+            required: true,
+        }
+      },
+      
+      // Specify validation error messages
+      messages: {
+        firstname: "Please enter your firstname",
+        password: {
+          required: "Please provide a password",
+          minlength: "Your password must be at least 5 characters long"
+        },
+        password1: {
+          required: "Please provide a password",
+          minlength: "Your password must be at least 5 characters long"
+        },
+        email: "Please enter a valid email address",
+        mobile: "please enter your valid mobile number",
+        gender:"please select your gender",
+        country:"please select your country",
+        password1:"please enter confirm password",
+        textarea:"Please fill your description"
+
+      },
+      
+      // Make sure the form is submitted to the destination defined
+      // in the "action" attribute of the form when valid
+      submitHandler: function(form) {
+          console.log("=========")
+        let fullname = document.getElementById("firstname").value;
+        let email = document.getElementById("email").value;
+        let mobile = document.getElementById("mobile").value;
+        let country = document.getElementById("country").value;
+        let comment = document.getElementById("comment").value;
+
+    
+        document.getElementById("outputname").innerText = fullname;
+        document.getElementById("outputemail").innerText = email;
+        document.getElementById("outputmnumber").innerText = mobile;
+        document.getElementById("outputcountry").innerText = country;
+        document.getElementById("outputdescription").innerText = comment;
+        form.submit();
+      }
+    });
+  });
+    
+});
